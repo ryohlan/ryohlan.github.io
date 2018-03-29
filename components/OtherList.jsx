f7b1c2e5cdeb50b7c19c3b6627383c2fb8dcdbc6
ryohlan <sunsunryohlan@gmail.com>
2018-03-29 15:14:09 +0900

    update others

--- components/OtherList.jsx
+++ components/OtherList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Styled from "styled-components";
 import { Colors, Values } from "../Assets";
 import Slide from "./Slide";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   others: Array<any>;
@@ -21,6 +22,49 @@ const SlidePage = Styled.div`
   height: 200px;
 `;
 
+const renderUdemy = markdown => (
+  <ReactMarkdown
+    source={markdown}
+    renderers={{
+      image: Styled.img`
+        height: 40px;
+        float: left;
+        margin-right: 1rem;
+      `,
+      heading: Styled.h1`
+        height: 40px;
+        display: flex;
+        align-items: center;
+        font-size: 1.1rem;
+        margin-bottom: 1rem;
+      `,
+      paragraph: Styled.p`
+        font-size: 0.8rem;
+        color: ${Colors.font.secondary};
+      `,
+      link: Styled.a.attrs({ target: "_blank", rel: "noopener" })`
+        color: ${Colors.font.secondary};
+        margin: 0.5rem 0.2rem 0;
+        float: right;
+        word-break: break-word;
+      `
+    }}
+  />
+);
+
+const OtherWrapper = Styled.div`
+  background-color: #FFF;
+  border-radius: 1px;
+  padding: 1.3rem;
+  margin: 2px;
+  display: flex;
+  flex: 1;
+  min-width: 30%;
+  @media(max-width: ${Values.SP_BREAK_POINT}) {
+    margin: 2px 0;
+  }
+`;
+
 export default ({ others }: Props) => (
   <React.Fragment>
     <section>
@@ -33,6 +77,14 @@ export default ({ others }: Props) => (
             id={s.number}
             slideMarkdowns={s.body.split("---")}
           />
+        ))}
+    </section>
+    <section style={{ marginTop: "1rem" }}>
+      <SectionTitle>Others</SectionTitle>
+      {others
+        .filter(s => !s.labels.find(s => s.name === "Slide"))
+        .map(s => (
+          <OtherWrapper key={s.id}>{renderUdemy(s.body)}</OtherWrapper>
         ))}
     </section>
   </React.Fragment>
